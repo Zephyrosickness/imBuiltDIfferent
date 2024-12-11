78a8eddf494831c558ad7348edd40ec010ed88d7
Zephyros <zxphyros@gmail.com>
2024-12-11 08:35:46 -0500

    BRO THERES SO MANY COMMENTS
    i already understood everything u didnt need all these comments LOL i mean i know that everyone else in programming doesnt get it because they arent built different like me smh

--- src/ReadingFiles.java
+++ src/ReadingFiles.java
@@ -10,79 +10,50 @@ public class  ReadingFiles {
     //need to add the "throws IOException" after typical main phrase
     public static void main(String[] args) throws IOException   {
 
-        //Creates a JFileChooser object that will open the JFileChooser Wizard GUI
-        //Allows user to search for files that they want read by the program
-        //Much easier for the user than typing in a file directory manually
+
         JFileChooser chooser = new JFileChooser();
-
-        //The try block will prompt the user to open a file
-        //If an error occurs in this try block, the catch block will handle the IO Exception
         try {
-            //This variable will hold the users current working directory (project folder)
-            //"user.dir" is shorthand for current working directory (project folder)
-            //This only assigns the address to the variable
-            File workingDirectory = new File(System.getProperty("user.dir"));
+            File workingDirectory = new File(System.getProperty("user.dir")); //this is the current directory
+            chooser.setCurrentDirectory(workingDirectory); //sets the default directory to the current one
+            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
+                File selectedFile = chooser.getSelectedFile(); //sets selected selectedFilePath to what the user clicked
+                Path selectedFilePath = selectedFile.toPath(); //this is the path of the selected selectedFilePath
 
-            //This will make the JFileChooser GUI default to look in the workingDirectory first
-            //This actually points the chooser at the desired address
-            //User can still navigate out of this folder if desired
-            chooser.setCurrentDirectory(workingDirectory);
-
-            //Checks to see if the user picks a file in the file chooser wizard
-            if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
-                //Stores user selected file
-                File selectedFile = chooser.getSelectedFile();
-                //Holds the path to the selected file (think of this as its address)
-                Path file = selectedFile.toPath();
-
-                //InputStream is needed in order to create our Buffered Reader
-                //InputStream allows bytes of data to be read from a file
-                //BufferedReader is our actual "reader" tool that will be used to read characters from file
-                InputStream in = new BufferedInputStream(Files.newInputStream(file, CREATE));
-                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                InputStream in = new BufferedInputStream(Files.newInputStream(selectedFilePath, CREATE)); //u need this to create a reader: this allows all bytes to be read
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in)); //this is the actual reader
 
                 /*
                     Everything starting here is what is actually displayed back to the user
                     This is typically where you will make changes.
                 */
 
-                //Prints File Path of selected File
-                System.out.println("File Path: " + file);
-                //Starts at line 0 and moves line by line through the file
+                System.out.println("File Path: " + selectedFilePath);
                 int line = 0;
-                //Rec holds what the reader finds on the line
-                String rec = "";
+                String rec = ""; //rec holds what the reader finds on the line
 
-                //Moving through file, reading, and printing each line of the selected file
+                //Moving through selectedFilePath, reading, and printing each line of the selected selectedFilePath
                 while (reader.ready()) {
                     rec = reader.readLine();
                     line++;
-                    //Prints the line # and the contents of the line
-                    System.out.printf("\nLine%4d: %-60s ", line, rec);
+                    System.out.printf("\nLine%4d: %-60s ", line, rec); //Prints the line # and the contents of the line
                 }
-                reader.close(); // must close the file to seal it and clear buffer
-                System.out.println("\n\nData file read!"); //Success message
+                reader.close(); // must close the selectedFilePath to seal it and clear buffer
+                System.out.println("\n\nData selectedFilePath read!"); //Success message
 
-                 /*
-                    This is where the program stops displaying the read file to the user
-                */
 
+                //we are in try/catch land now after this line
             } else {
                 //This else statement is hit when the user closes the JFileChooser Wizard without selecting file
                 System.out.println("File not selected. Please restart program.");
-                System.exit(0); //Shuts down program
+                System.exit(0);
             }
         }
         //This catch block is hit when the user file the user attempts to open a file that can not be found
         catch (FileNotFoundException e) {
             System.out.println("File not found!");
-            //Prints the error along with additional info related to the error
             e.printStackTrace();
         }
         //This catch block is hit for all other IO Exceptions
-        catch (IOException e) {
-            //Prints the error along with additional info related to the error
-            e.printStackTrace();
-        }
+        catch (IOException e) {e.printStackTrace();}
     }
 }
--- src/WritingFiles.java
+++ src/WritingFiles.java
@@ -1,4 +1,3 @@
-//Allows us to use the FileChooser wizard GUI to pick files
 import javax.swing.*;
 //Needed imports for working w/ IO (input/output)
 import java.io.*;
@@ -9,7 +8,6 @@ import java.util.ArrayList;
 import static java.nio.file.StandardOpenOption.CREATE;
 
 public class  WritingFiles {
-    //need to add the "throws IOException" after typical main phrase
     public static void main(String[] args) throws IOException   {
 
         //Sample data that is being added to an ArrayList named recs
@@ -18,20 +16,11 @@ public class  WritingFiles {
         recs.add("Sample data Line 2");
         recs.add("Sample data Line 3");
 
-        //This variable will hold the users current working directory (project folder)
-        //"user.dir" is shorthand for current working directory (project folder)
+
         File workingDirectory = new File(System.getProperty("user.dir"));
-        //Path is automatically set for user
-        //In this case, the file will be stored in the src folder and the name is already chosen
-        //You may want to change the file name or give the user the ability to choose their own file name
         Path file = Paths.get(workingDirectory.getPath() + "\\src\\data.txt");
 
-        //The try block will attempt to write a new txt file
-        //If an error occurs in this block, the catch block will handle the IO Exception
         try {
-            //OutputStream is needed in order to create our Buffered Writer
-            //OutputStream allows bytes of data to be written to a file
-            //BufferedWriter is our actual writing tool that will be used to write characters to file
             OutputStream out = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
 
@@ -44,13 +33,10 @@ public class  WritingFiles {
                 //need new line added before we write more data - ensures next bit of data is put on own line
                 writer.newLine();
             }
-            writer.close(); //closes file and clears buffer
+            writer.close();
             System.out.println("Data file written!");
         }
         //This catch block is hit for a variety of IO Exceptions
-        catch (IOException e) {
-            //Prints the error along with additional info related to the error
-            e.printStackTrace();
-        }
+        catch (IOException e) {e.printStackTrace();}
     }
 }
